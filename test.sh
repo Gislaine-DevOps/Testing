3b21f987c9fa7cdc61db0950c48edeaf17738149
Gislaine <mgislaine2@yahoo.fr>
2022-02-19 15:55:20 -0600

    Testing

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+name=Gislaine
+company=EvaSarl
+passwprd=Eva123
+
+echo "Hello my name is $name"
+echo "I am a DevOps Engineer in $company"
+echo "My password is"
+read password
+echo "Welcome to everyone who enter in our company"
+echo "$name"
+date
